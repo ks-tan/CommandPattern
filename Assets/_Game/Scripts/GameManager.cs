f2ae2792bf06a61854bbb651239262c9218d8ebf
Tan Kang Soon <tankangsoon@gmail.com>
2023-12-02 17:57:26 +0800

    Move other jammos!

--- Assets/_Game/Scripts/GameManager.cs
+++ Assets/_Game/Scripts/GameManager.cs
@@ -1,19 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private InputManager _inputManager;
     [SerializeField] private JammoController _playerController;
+    [SerializeField] private JammoController _jammoPrefab;
+
+    // For tracking replays
+    private Dictionary<JammoController, Command[]> _jammoToCommandsMap = new Dictionary<JammoController, Command[]>();
+    private Dictionary<JammoController, int> _jammoToCommandIndex = new Dictionary<JammoController, int>();
+    private Dictionary<JammoController, float> _jammoCreationTimes = new Dictionary<JammoController, float>();
 
     private void Update()
     {
         if (_inputManager.TryGetInput(out Command input))
         {
-            if (input.Action == Command.ActionType.OPTION_0)
+            if (input.Action == Command.ActionType.OPTION_0 && input.State == Command.KeyState.DOWN)
             {
-                // TODO: Get all commands in InputManager's history and play it on a new JammoController
+                // Get all commands in InputManager's history and map it to a new JammoController
+                var newJammo = Instantiate(_jammoPrefab);
+                newJammo.Reset();
                 Command[] commandHistory = new Command[_inputManager.CommandHistory.Count];
                 _inputManager.CommandHistory.CopyTo(commandHistory, 0);
+                _jammoToCommandsMap.Add(newJammo, commandHistory);
+                _jammoToCommandIndex.Add(newJammo, 0);
+                _jammoCreationTimes.Add(newJammo, Time.time);
 
                 // Reset player's position and clear InputManager's command history
                 _inputManager.ClearHistory();
@@ -23,10 +35,26 @@ public class GameManager : MonoBehaviour
             else _playerController.ReadCommand(input);
         }
 
+        // Feed commands to all other jammos
+        foreach (var jammo in _jammoToCommandsMap.Keys)
+        {
+            var currentTimeStep = Time.time - _jammoCreationTimes[jammo];
+            var commandHistory = _jammoToCommandsMap[jammo];
+            var currentCommandIndex = _jammoToCommandIndex[jammo];
+            var currentCommand = commandHistory[currentCommandIndex];
+            if (currentTimeStep < currentCommand.Time) continue;
+            jammo.ReadCommand(currentCommand);
+            _jammoToCommandIndex[jammo]++;
+        }
     }
 
     private void FixedUpdate()
     {
+        // FixedUpdate player controller
         _playerController.FixedUpdateController();
+
+        // FixedUpdate all other jammos
+        foreach (var jammo in _jammoToCommandsMap.Keys)
+            jammo.FixedUpdateController();
     }
 }
